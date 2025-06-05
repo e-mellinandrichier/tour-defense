1175fd8e62859aa60512d67d9c125b407528cdfb
jerome-cossu <jerome.cossu@laplateforme.io>
2025-06-05 11:26:06 +0200

    ✨ feat : add on till follow the path with life bar

---
+++ build/CMakeFiles/TowerDefense.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TowerDefense.dir/main.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/main.cpp.obj.d"
+  "CMakeFiles/TowerDefense.dir/models/dino.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/models/dino.cpp.obj.d"
+  "CMakeFiles/TowerDefense.dir/models/map.cpp.obj"
+  "CMakeFiles/TowerDefense.dir/models/map.cpp.obj.d"
+  "TowerDefense.exe"
+  "TowerDefense.exe.manifest"
+  "TowerDefense.pdb"
+  "libTowerDefense.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TowerDefense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TowerDefense.dir/main.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/TowerDefense.dir/main.cpp.obj: \
+ C:\Users\jerom\OneDrive\Bureau\C-C++\tour-defense\main.cpp \
+ C:\Users\jerom\OneDrive\Bureau\C-C++\tour-defense\models/map.hpp
